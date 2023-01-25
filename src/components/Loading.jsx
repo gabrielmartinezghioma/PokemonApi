c6238b0bf0a29ac0d05f3033e44b06e8b32b594c
GHIOMA <martinezgabrielalejandro4@gmail.com>
2023-01-25 19:45:56 -0300

    pokemons per page

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = () => {
   return (
     <div className='div--isLoading'>
-      <img className='div__img--isLoading' src="/public/pokemon-pokeball.gif" alt="pokemon-pokebeball" />
+      <img className='div__img--isLoading' src="pokemon-pokeball.gif" alt="pokemon-pokebeball" />
       <h2 className='div__h2--isLoading'>Loading...</h2>
     </div>
   );
