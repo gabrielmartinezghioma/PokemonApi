3dbedf8729422d3c4bc3594f6ddf1781d2b22a67
GHIOMA <martinezgabrielalejandro4@gmail.com>
2023-01-25 19:44:02 -0300

    update

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -5,7 +5,7 @@ const Error404 = () => {
     <div className='div--isError'>
       <div className='div__div--isError'>
         <h2 className='div__div--h2IsError'>4</h2>
-        <img className='div__div--imgIsError' src="/public/pokeball.png" alt="pokeball" />
+        <img className='div__div--imgIsError' src="pokeball.png" alt="pokeball" />
         <h2 className='div__div--h2IsError'>4</h2>
       </div>
       <h2 className='div__h2--isError'>Ups, no se han encontrado resultados...</h2>
