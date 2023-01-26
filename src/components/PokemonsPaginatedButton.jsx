b0c13a191090e38e303d4cfd08f51ce37a41df58
GHIOMA <martinezgabrielalejandro4@gmail.com>
2023-01-26 11:08:34 -0300

    setting css

--- src/components/PokemonsPaginatedButton.jsx
+++ src/components/PokemonsPaginatedButton.jsx
@@ -5,6 +5,7 @@ const PokemonsPaginatedButton = ({ numberPage, functionSet }) => {
     <div className='div__selectionPerPage'>
       <img className='div__img--selectionPerPage' src="/pokeball.png" alt="" />
       <div className='div__div--selectionPerPage'>
+        <button className='div__div--buttonSelectionPerPage' onClick={() => functionSet(numberPage * 2)}>{numberPage * 2}</button>
         <button className='div__div--buttonSelectionPerPage' onClick={() => functionSet(numberPage * 4)}>{numberPage * 4}</button>
         <button className='div__div--buttonSelectionPerPage' onClick={() => functionSet(numberPage * 6)}>{numberPage * 6}</button>
         <button className='div__div--buttonSelectionPerPage' onClick={() => functionSet(numberPage * 8)}>{numberPage * 8}</button>
